af298a406d3e5fc4a9997a4e0be3a5b21d746417
Ismail Mayat <imayat@thecogworks.co.uk>
2014-09-29 17:20:26 +0100

    ex 2 complete

---
+++ ExamineBootcamp.BusinessLogic/Events/ExamineEvents.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Examine;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
+using UmbracoExamine;
+
+namespace ExamineBootcamp.BusinessLogic.Events
+{
+    public class ExamineEvents : IApplicationEventHandler
+    {
+        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+ 
+        }
+
+        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+
+        }
+
+        public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+             ExamineManager.Instance.IndexProviderCollection["ExternalIndexer"].GatheringNodeData +=
+                ExternalIndexerGatheringNodeData;
+        }
+
+        private void ExternalIndexerGatheringNodeData(object sender, IndexingNodeDataEventArgs e)
+        {
+            if (e.IndexType == IndexTypes.Content)
+            {             
+                try
+                {
+                    var fields = e.Fields;
+                    var combinedFields = new StringBuilder();
+                    foreach (var keyValuePair in fields)
+                    {
+                        combinedFields.AppendLine(keyValuePair.Value);
+                    }
+                    e.Fields.Add("contents", combinedFields.ToString());             
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.Error<Exception>("error munging fields for " + e.NodeId,ex);
+                }
+            }
+        }
+    }
+}

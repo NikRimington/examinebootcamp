b0c51d6b84ec57b7ac4b593907d3075301df47ea
Ismail Mayat <imayat@thecogworks.co.uk>
2014-09-30 17:45:13 +0100

    ex4 done

---
+++ ExamineBootcamp.BusinessLogic/Helper/ExamineHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExamineBootcamp.BusinessLogic.Helper
+{
+    public static class ExamineHelper
+    {
+        /// <summary>
+        /// take from http://stackoverflow.com/questions/263081/how-to-make-the-lucene-queryparser-more-forgiving
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public static string MakeSearchQuerySafe(this string query)
+        {
+            var regex = new Regex(@"[^\w\s-]");
+            return regex.Replace(query, "");
+        }
+    }
+}

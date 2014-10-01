bbecb4dde8703d29489fdb0cd133c7e024c78b61
Ismail Mayat <imayat@thecogworks.co.uk>
2014-10-01 10:54:06 +0100

    ex 6

--- ExamineBootcamp.BusinessLogic/Events/ExamineEvents.cs
+++ ExamineBootcamp.BusinessLogic/Events/ExamineEvents.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Examine;
+using Lucene.Net.Documents;
 using Umbraco.Core;
 using Umbraco.Core.Logging;
 using UmbracoExamine;
@@ -26,6 +28,31 @@ namespace ExamineBootcamp.BusinessLogic.Events
         {
              ExamineManager.Instance.IndexProviderCollection["ExternalIndexer"].GatheringNodeData +=
                 ExternalIndexerGatheringNodeData;
+
+             var indexer = (UmbracoContentIndexer)ExamineManager.Instance.IndexProviderCollection["ExternalIndexer"];
+
+             indexer.DocumentWriting += indexer_DocumentWriting;
+        }
+
+        void indexer_DocumentWriting(object sender, Examine.LuceneEngine.DocumentWritingEventArgs e)
+        {
+            const string dateField = "reviewDate";
+
+                DateTime articleDate;
+                if (e.Fields.ContainsKey(dateField))
+                {
+                    articleDate = DateTime.Parse(e.Fields[dateField]);
+                }
+                else
+                {
+                    articleDate = DateTime.Parse(e.Fields["updateDate"]);
+                }
+                // in __Sort_articleDate the __ implies is not analysed therefore can be used to sorting. sorted means its retrievable
+                //see page 43 lucene in action 2nd edition for full explanation of options
+                var field = new Field("__Sort_" + dateField, articleDate.ToString("yyyyMMddHHmmss", CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NOT_ANALYZED);
+
+                e.Document.Add(field);
+            
         }
 
         private void ExternalIndexerGatheringNodeData(object sender, IndexingNodeDataEventArgs e)
